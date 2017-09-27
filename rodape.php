1e3441dae94574d9aa541666554b9ea25ea7bbab
Andrew Kevin Ferreira Rocha <32345470+AndrewKeviin@users.noreply.github.com>
2017-09-27 19:54:17 -0300

    Update rodape.php

--- rodape.php
+++ rodape.php
@@ -1,4 +1,15 @@
 </div>
+
 </div>
+<footer class="rodape">
+	<footer class="titulo">
+		<h2>Ristorante per Famiglie</h2>
+	</footer>
+	<footer class="informacao">
+			<p><b>ENDEREÇO<b>: CEP 62345-123, Avenida dos Mafiosos,Bloco F,  Edifísio: EL Chefão</br>
+			<b>MAIS INFORMAÇÕES<b>: (28)4321-1234 / (28)1234-4321</p>
+	</footer>
+</footer>
+
 </body>
 </html>
